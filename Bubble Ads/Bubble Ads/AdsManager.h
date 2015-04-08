09a7ef21022233b622cb45cd20c03457ed75c0bc
Amr Alassal <amr.alassal@inovaeg.com>
2015-04-08 18:26:54 +0100

    Added Ads Manager class

---
+++ Bubble Ads/Bubble Ads/AdsManager.h
@@ -0,0 +1,13 @@
+//
+//  AdsManager.h
+//  Bubble Ads
+//
+//  Created by Inova5 on 4/8/15.
+//  Copyright (c) 2015 Kareem. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AdsManager : NSObject
+
+@end
---
+++ Bubble Ads/Bubble Ads/AdsManager.m
@@ -0,0 +1,14 @@
+//
+//  AdsManager.m
+//  Bubble Ads
+//
+//  Created by Inova5 on 4/8/15.
+//  Copyright (c) 2015 Kareem. All rights reserved.
+//
+
+#import "AdsManager.h"
+
+@implementation AdsManager
+
+
+@end
